76fccfc0a97323bf32014f847c3020d22b8a7820
Zoltan <zoltan.csonka@railsbank.com>
2021-10-08 11:28:05 +0200

    cause error

--- models/dim_customer.sql
+++ models/dim_customer.sql
@@ -1,6 +1,7 @@
 select
     customer.dim_customer_id,
     customer.customer_name,
-    customer.is_active,
-    customer.created_at
+    customer.is_active
+        customer.created_at
+
 from customer
